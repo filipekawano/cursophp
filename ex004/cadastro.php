f5f9a7cc56ed709ede1b04a44a91ce817f220ac7
Filipe Kawano <filipekawan@hotmail.com>
2025-01-02 22:03:19 -0300

    Formulario criado

--- ex004/cadastro.php
+++ ex004/cadastro.php
@@ -1,1 +1,21 @@
-Olá, mundo!
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php 
+            $name = $_GET["nome"];
+            $surname = $_GET["sobrenome"];
+            echo "<p>É um prazer te conhecer, $name $surname! Este é o meu site."
+        ?>
+    </main>
+</body>
+</html>
