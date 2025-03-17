c593ce1ac05e81e49a6191b3f67e4de88a6af84d
Filipe Kawano <filipekawan@hotmail.com>
2025-03-17 03:40:11 -0300

    Atualizado

---
+++ 8_INCLUSAO_DE_ARQUIVO/exibicao_de_conteudo/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $nome = "Filipe";
+
+?>
+
+<form action="">
+    <input type="text" value="<?= $nome; ?>">
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
